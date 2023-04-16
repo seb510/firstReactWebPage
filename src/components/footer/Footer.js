1f05717867f06b563833c016c01fec55a7ac103a
Nikolay <34944975+seb510@users.noreply.github.com>
2023-04-16 21:16:09 +0300

    new files

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,50 @@
+import vk from "../../img/icons/vk.svg";
+import instagram from "../../img/icons/instagram.svg";
+import twitter from "../../img/icons/twitter.svg";
+import githab from "../../img/icons/gitHub.svg";
+import linkedin from "../../img/icons/linkedIn.svg";
+
+import './style.css'
+
+const Footer = () => {
+  return(
+      <footer className="footer">
+          <div className="container">
+              <div className="footer__wrapper">
+                  <ul className="social">
+                      <li className="social__item">
+                          <a href="#!" target={"_blank"}>
+                              <img src={vk} alt="Link"/>
+                          </a>
+                      </li>
+                      <li className="social__item">
+                          <a href="#!" target={"_blank"}>
+                              <img src={instagram} alt="Link"/>
+                          </a>
+                      </li>
+                      <li className="social__item">
+                          <a href="#!" target={"_blank"}>
+                              <img src={twitter} alt="Link"/>
+                          </a>
+                      </li>
+                      <li className="social__item">
+                          <a href="#!" target={"_blank"}>
+                              <img src={githab} alt="Link"/>
+                          </a>
+                      </li>
+                      <li className="social__item">
+                          <a href="#!" target={"_blank"}>
+                              <img src={linkedin} alt="Link"/>
+                          </a>
+                      </li>
+                  </ul>
+                  <div className="copyright">
+                      <p>© 2022 frontend-dev.com</p>
+                  </div>
+              </div>
+          </div>
+      </footer>
+  );
+}
+
+export default Footer;
